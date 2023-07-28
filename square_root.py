39468e337022213401bb766cc5d63bcf55b5245d
Priya Pandey <118142904+pandey-priya@users.noreply.github.com>
2023-07-27 23:57:21 -0700

    Create square_root.py

---
+++ square_root.py
@@ -0,0 +1,7 @@
+# Python Program to calculate the square root
+
+# To take the input from the user
+num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.1f is %0.1f'%(num ,num_sqrt))
