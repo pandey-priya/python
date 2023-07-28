8d17314ef58ee883f33ea35b56266eb9ee3ecddb
Priya Pandey <118142904+pandey-priya@users.noreply.github.com>
2023-07-27 23:27:23 -0700

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,8 @@
+#This code takes two numbers from the user, adds them together and prints out the sum
+
+num1 = float(input('What is the first number?: '))
+num2 = float(input('What is the second number?: '))
+
+sum = num1 + num2
+
+print ('The sum of %0.1f and %0.1f is %0.1f' %(num1,num2,sum))
