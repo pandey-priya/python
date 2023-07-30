3daa36eafc09f9e0118226b76d42d069b1644ba3
Priya Pandey <118142904+pandey-priya@users.noreply.github.com>
2023-07-29 20:38:05 -0700

    Create pos_neg_zero_num_check.py

---
+++ pos_neg_zero_num_check.py
@@ -0,0 +1,10 @@
+#Taking input from the user
+num = float(input("Enter a number: "))
+
+#Checking condition for a number to be +ve, -ve or zero
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
